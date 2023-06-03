d52f27b3a6fc3d011662da3b23b18d8fc35903bb
Fayozxon <83503559+Fayozxon@users.noreply.github.com>
2023-06-03 18:18:55 +0500

    added meta tags

--- src/views/TechnologyView.vue
+++ src/views/TechnologyView.vue
@@ -1,5 +1,29 @@
 <script>
+export default {
+    data() {
+        return {
+            technologies: [],
+            technology: '',
+            activeTab: 0
+        }
+    },
+    methods: {
+        async getTechnologyData() {
+            let res = await fetch('data/technology.json');
+            let data = await res.json();
 
+            this.technologies = data;
+        }
+    },
+    computed: {
+        getInformation() {
+            return this.technologies[this.activeTab];
+        }
+    },
+    created() {
+        this.getTechnologyData();
+    }
+}
 </script>
 
 <template>
@@ -9,7 +33,17 @@
                 <span class="number">04</span>
                 SPACE LAUNCH 101
             </h5>
-            <h1>Technology!</h1>
+
+            <div class="destinations-section__tab" v-if="technologies.length">
+                <!-- preloading images -->
+                <img
+                    v-for="technology in technologies"
+                    :src="technology.images.portrait"
+                    style="display: ;"
+                    preload
+                >
+            </div>
+
         </div>
     </section>
 </template>
